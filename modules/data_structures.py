bdeb550d2da524fbd4f246785672aae8d4efdfbd
Jerik <jerik+opensource@mailbox.org>
2022-08-19 18:10:36 +0200

    Initial version of LSI notebook. Geometry: Add line segments. Data Structures: Implement balanced binary tree supporting operations necessary for LSI plane sweep.

---
+++ modules/data_structures.py
@@ -0,0 +1,278 @@
+from typing import TypeVar, Generic, Optional, Iterable, Callable, Any
+from enum import Enum
+from itertools import chain
+from abc import ABC, abstractmethod
+
+
+K = TypeVar("K")
+V = TypeVar("V")
+
+Updater = Callable[[Optional[V]], V]
+
+class ComparisonResult(Enum):
+    SMALLER = -1
+    EQUAL = 0
+    GREATER = 1
+
+class Comparator(ABC, Generic[K]):
+    @abstractmethod
+    def __call__(self, key: K, item: Any) -> ComparisonResult:
+        pass
+
+class DefaultComparator(Generic[K]):
+    def __call__(self, key: K, item: Any) -> ComparisonResult:
+        if key < item:
+            return ComparisonResult.SMALLER
+        elif key == item:
+            return ComparisonResult.EQUAL
+        elif key > item:
+            return ComparisonResult.GREATER
+        # TODO: raise Error otherwise
+
+
+class BinaryTree(Generic[K, V]):
+    def __init__(self, comparator: Comparator[K] = DefaultComparator[K]()):
+        self.root: Node[K, V] = Node.empty_node()
+        self.comparator = comparator
+
+    def is_empty(self) -> bool:
+        return self.root.is_empty()
+
+    def insert(self, key: K, value: V) -> bool:
+        return self.root.insert(key, value, self.comparator)
+
+    def update(self, key: K, updater: Updater[V]) -> bool:
+        return self.root.update(key, updater, self.comparator)
+
+    def delete(self, key: K) -> Optional[V]:
+        return self.root.delete(key, self.comparator)
+
+    def pop_smallest(self) -> tuple[K, V]:
+        return self.root.pop_smallest(self.comparator)
+
+    def smaller_neighbour(self, item: Any) -> Optional[K]:
+        return self.root.smaller_neighbour(item, self.comparator)
+
+    def greater_neighbour(self, item: Any) -> Optional[K]:
+        return self.root.greater_neighbour(item, self.comparator)
+
+    def range_between_neighbours(self, item: Any) -> list[K]:      # TODO: is list conversion necessary?
+        return list(self.root.range_between_neighbours(item, self.comparator))
+
+    def __repr__(self) -> str:
+        return self.root.__repr__()
+
+
+class Node(Generic[K, V]):
+    def __init__(self, key: K, value: V, level: int, left: 'Node[K, V]', right: 'Node[K, V]'):
+        self.key = key
+        self.value = value
+        self.level = level
+        self.left = left
+        self.right = right
+
+    @classmethod
+    def empty_node(cls) -> 'Node':
+        return cls(None, None, 0, None, None)
+
+    def make_empty(self):
+        self.key = None
+        self.value = None
+        self.level = 0
+        self.left = None
+        self.right = None
+
+    def is_empty(self) -> bool:
+        return self.level == 0      # TODO: maybe use "left is None and right is None" instead...
+
+    def make_leaf(self, key: K, value: V):
+        self.key = key
+        self.value = value
+        self.level = 1
+        self.left = Node.empty_node()
+        self.right = Node.empty_node()
+
+    def is_leaf(self) -> bool:
+        #return self.level == 1     # THIS IS WRONG
+        return not self.is_empty() and self.left.is_empty() and self.right.is_empty()
+
+    def get_successor(self) -> 'Node[K,V]':     # fails if self or self.right is empty... TODO: Add checks?
+        successor = self.right
+        while not successor.left.is_empty():
+            successor = successor.left
+        return successor
+
+    def get_predecessor(self) -> 'Node[K,V]':   # fails if self or self.left is empty... TODO: Add checks?
+        predecessor = self.left
+        while not predecessor.right.is_empty():
+            predecessor = predecessor.right
+        return predecessor
+
+    def replace_with_successor(self, comparator: Comparator[K]):
+        successor = self.get_successor()
+        self.key = successor.key
+        self.value = successor.value
+        self.right.delete(successor.key, comparator)
+
+    def replace_with_predecessor(self, comparator: Comparator[K]):
+        predecessor = self.get_predecessor()
+        self.key = predecessor.key
+        self.value = predecessor.value
+        self.left.delete(predecessor.key, comparator)
+
+    def skew(self):
+        if self.is_empty() or self.left.is_empty():
+            return
+        
+        if self.left.level == self.level:
+            self.right = Node(self.key, self.value, self.level, self.left.right, self.right)   # TODO: remove allocation?
+            self.key = self.left.key
+            self.value = self.left.value
+            self.left = self.left.left
+
+    def split(self):
+        if self.is_empty() or self.right.is_empty() or self.right.right.is_empty():
+            return
+
+        if self.right.right.level == self.level:
+            self.left = Node(self.key, self.value, self.level, self.left, self.right.left)     # TODO: remove allocation?
+            self.key = self.right.key
+            self.value = self.right.value
+            self.level = self.right.level + 1
+            self.right = self.right.right
+
+    def adjust_after_deletion(self):
+        if self.is_empty():
+            return
+
+        level = min(self.left.level, self.right.level) + 1
+        if level < self.level:
+            self.level = level
+            if level < self.right.level:
+                self.right.level = level
+        
+        self.skew()
+        self.right.skew()
+        if not self.right.is_empty():
+            self.right.right.skew()
+        self.split()
+        self.right.split()
+
+    def insert(self, key: K, value: V, comparator: Comparator[K]) -> bool:     # TODO: another method for overwriting insertions returning the old value?
+        if self.is_empty():
+            self.make_leaf(key, value)
+            return False
+        
+        cr = comparator(self.key, key)
+        if cr is ComparisonResult.GREATER:
+            was_key_present = self.left.insert(key, value, comparator)
+        elif cr is ComparisonResult.SMALLER:
+            was_key_present = self.right.insert(key, value, comparator)
+        else:
+            was_key_present = True
+
+        if not was_key_present:
+            self.skew()
+            self.split()
+        return was_key_present
+
+    def update(self, key: K, updater: Updater[V], comparator: Comparator[K]) -> bool:
+        if self.is_empty():
+            self.make_leaf(key, updater(None))
+            return False
+        
+        cr = comparator(self.key, key)
+        if cr is ComparisonResult.GREATER:
+            was_key_present = self.left.update(key, updater, comparator)
+        elif cr is ComparisonResult.SMALLER:
+            was_key_present = self.right.update(key, updater, comparator)
+        else:
+            self.value = updater(self.value)
+            was_key_present = True
+
+        if not was_key_present:
+            self.skew()
+            self.split()
+        return was_key_present
+
+    def delete(self, key: K, comparator: Comparator[K]) -> Optional[V]:
+        if self.is_empty():
+            return None
+        
+        cr = comparator(self.key, key)
+        if cr is ComparisonResult.GREATER:
+            value = self.left.delete(key, comparator)
+        elif cr is ComparisonResult.SMALLER:
+            value = self.right.delete(key, comparator)
+        else:
+            value = self.value
+            if self.is_leaf():
+                self.make_empty()
+            elif self.left.is_empty():
+                self.replace_with_successor(comparator)
+            else:
+                self.replace_with_predecessor(comparator)
+
+        self.adjust_after_deletion()        # TODO: this is only necessary if a deletion occured...
+        return value
+
+    def pop_smallest(self, comparator: Comparator[K]) -> tuple[K, V]:
+        if self.is_empty():
+            return None      # TODO: maybe throw assertion instead...
+
+        if not self.left.is_empty():
+            key, value = self.left.pop_smallest(comparator)
+        else:
+            key = self.key
+            value = self.value
+            if self.is_leaf():
+                self.make_empty()
+            else:
+                self.replace_with_successor(comparator)
+        
+        self.adjust_after_deletion()
+        return key, value
+
+    def smaller_neighbour(self, item: Any, comparator: Comparator[K], previous_key: Optional[K] = None) -> Optional[K]:   # TODO: Also return value?
+        if self.is_empty():
+            return previous_key
+
+        if comparator(self.key, item) is ComparisonResult.SMALLER:
+            return self.right.smaller_neighbour(item, comparator, self.key)
+        else:
+            return self.left.smaller_neighbour(item, comparator, previous_key)
+
+    def greater_neighbour(self, item: Any, comparator: Comparator[K], previous_key: Optional[K] = None) -> Optional[K]:   # TODO: Also return value?
+        if self.is_empty():
+            return previous_key
+
+        if comparator(self.key, item) is ComparisonResult.GREATER:
+            return self.left.greater_neighbour(item, comparator, self.key)
+        else:
+            return self.right.greater_neighbour(item, comparator, previous_key)
+
+    def range_between_neighbours(self, item: Any, comparator: Comparator[K]) -> Iterable[K]:      # running time is in O(log(n) * |output|)
+        if self.is_empty():
+            return ()
+
+        cr = comparator(self.key, item)
+        if cr is ComparisonResult.GREATER:
+            return self.left.range_between_neighbours(item, comparator)
+        elif cr is ComparisonResult.SMALLER:
+            return self.right.range_between_neighbours(item, comparator)
+        else:
+            return chain(
+                self.left.range_between_neighbours(item, comparator),
+                (self.key,),
+                self.right.range_between_neighbours(item, comparator)
+            )
+
+    def __repr__(self) -> str:
+        if self.is_empty():
+            return ", "
+        if self.value:
+            return f"{self.key}/{self.value} <{self.left.__repr__()}> <{self.right.__repr__()}>"
+            #return f"{self.left.__repr__()}{self.key}:{self.value}{self.right.__repr__()}"
+        else:
+            return f"{self.key} <{self.left.__repr__()}> <{self.right.__repr__()}>"
+            #return f"{self.left.__repr__()}{self.key}{self.right.__repr__()}"
--- modules/geometry.py
+++ modules/geometry.py
@@ -2,7 +2,7 @@ from enum import Enum, auto
 import math
 import numpy as np
 
-from typing import Iterable, Union
+from typing import Iterable, Union, Any
 from abc import ABC, abstractmethod
 
 
@@ -15,7 +15,7 @@ class Orientation(Enum):
 
 
 class Point:
-    def __init__(self,x,y):
+    def __init__(self, x, y):
         #self.x = x
         #self.y = y
         self._coords = np.array([x, y])
@@ -28,17 +28,17 @@ class Point:
     def y(self):
         return self._coords[1]
         
-    def __eq__(self, other):
+    def __eq__(self, other) -> bool:
         return self.x == other.x and self.y == other.y
     
-    def __hash__(self):
+    def __hash__(self) -> int:
         return hash((self.x, self.y))
     
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"Point({self.x}, {self.y})"
     
-    def distance(self, other: 'Point') -> float:
-        return math.sqrt((self.x - other.x)**2 + (self.y - other.y)**2)
+    #def distance(self, other: 'Point') -> float:
+    #    return math.sqrt((self.x - other.x)**2 + (self.y - other.y)**2)
     
     def orientation(self, source: 'Point', target: 'Point') -> Orientation:
         if source == target:
@@ -76,7 +76,7 @@ class PointReference(Point):
     def y(self):
         return self.get_point().y
 
-    def get_position(self):
+    def get_position(self) -> int:
         return self._position
     
     def is_in_container(self, container: list[Point]) -> bool:
@@ -138,7 +138,7 @@ class Polygon:
         del self.points[key]
         self.events.append(DeleteEvent(key))
 
-    def __add__(self, other: 'Polygon'):
+    def __add__(self, other: 'Polygon') -> 'Polygon':
         result = Polygon()
         result.points = self.points + other.points
         result.events = self.events + other.events
@@ -182,3 +182,61 @@ class DeleteEvent(Event):
 
     def execute_on(self, points: list[Point], background_points: list[Point]):
         del points[self.key]
+
+
+
+
+class LineSegment:
+    def __init__(self, p: Point, q: Point):
+        if p == q:
+            raise ValueError("Line (segment) needs two different points")
+        if p.y > q.y or (p.y == q.y and p.x < q.x):     # TODO: maybe make is_upper method or similar (see [CG, p. 24] for order)
+            self.upper = p
+            self.lower = q
+        else:
+            self.upper = q
+            self.lower = p
+
+    def intersection(self, other: 'LineSegment') -> Union[None, Point, 'LineSegment']:
+        a = self.upper      # TODO: maybe implement add, subtract, ... on Point
+        b = self.lower
+        c = other.upper
+        d = other.lower
+        if (a.x - b.x) * (c.y - d.y) == (c.x - d.x) * (a.y - b.y):  # Line segments are parallel. TODO: Check for overlap. (-> degenerate case)
+            return None
+        elif a.y == b.y:
+            point = other.get_point_at_same_height(a)
+            if a.x <= point.x <= b.x:
+                return point
+            else:
+                return None
+        else:
+            coefficent = (c.x - d.x) - (b.x - a.x) * (c.y - d.y) / (b.y - a.y)
+            t = ((c.x - a.x) - (b.x - a.x) * (c.y - a.y) / (b.y - a.y)) / coefficent
+            #s = (c.y - a.y) / (b.y - a.y) - t * (c.y - d.y) / (b.y - a.y)
+            s = ((c.y - a.y) - t * (c.y - d.y)) / (b.y - a.y)
+            if 0 <= s <= 1 and 0 <= t <= 1:
+                #p1 = Point((1 - s) * a.x + s * b.x, (1 - s) * a.y + s * b.y)
+                #p2 = Point((1 - t) * c.x + t * d.x, (1 - t) * c.y + t * d.y)
+                p1 = Point(a.x + s * (b.x - a.x), a.y + s * (b.y - a.y))
+                p2 = Point(c.x + t * (d.x - c.x), c.y + t * (d.y - c.y))
+                #print(f"{p1} vs {p2}")                                   # TODO: p1 and p2 can differ... (-> not robust)
+                return p2
+            else:
+                return None
+
+    def get_point_at_same_height(self, point: Point) -> Point:          # TODO: Add checks?
+        scale = (point.y - self.lower.y) / (self.upper.y - self.lower.y)
+        x = self.lower.x + scale * (self.upper.x - self.lower.x)
+        return Point(x, point.y)
+
+    def __eq__(self, other: Any) -> bool:
+        if not isinstance(other, LineSegment):
+            raise TypeError("Need LineSegment")
+        return self.upper == other.upper and self.lower == other.lower
+
+    def __hash__(self) -> int:
+        return hash((self.upper, self.lower))
+
+    def __repr__(self) -> str:
+        return f"{self.upper}--{self.lower}"
