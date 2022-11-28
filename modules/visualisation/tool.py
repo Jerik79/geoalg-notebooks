325d3e88278f9b2740c5ac9dd1614173bfdd14c1
Jerik <jerik+opensource@mailbox.org>
2022-11-28 18:55:17 +0100

    LSI: Add images. Visualisation/tool: Enable displaying relevant static images on demand.

--- modules/visualisation/tool.py
+++ modules/visualisation/tool.py
@@ -27,12 +27,15 @@ class VisualisationTool(Generic[T]):
 
     ## Initialisation methods.
 
-    def __init__(self, width: int, height: int, instance: InstanceHandle[T]):
+    def __init__(self, width: int, height: int, instance: InstanceHandle[T], notebook_number: Optional[int] = None):
         self._width = width
         self._height = height
 
         self._instance = instance
         self._number_of_points = 0
+
+        self._notebook_number = notebook_number
+        self._next_image_number = 1
 
         self._multi_canvas = MultiCanvas(6, width = self._width, height = self._height)
         self._init_canvases()
@@ -43,7 +46,7 @@ class VisualisationTool(Generic[T]):
                 return
             if self.add_point(Point(x, self._height - y)):
                 self.clear_algorithm_output()
-                self._clear_message_labels()
+                self.clear_message_labels()
         self._multi_canvas.on_mouse_down(handle_click_on_canvas)
 
         self._canvas_output = Output(layout = Layout(border = "1px solid black"))
@@ -152,23 +155,22 @@ class VisualisationTool(Generic[T]):
     ## Widget display and manipulation methods.
 
     def display(self):
-        def vbox_with_header(title: str, children: Iterable[Widget], right_aligned: bool = False) -> VBox:
-            header = HTML(f"<h2>{title}</h2>", layout = Layout(align_self = "flex-start"))
-            vbox_layout = Layout(padding = "0px 25px", align_items = "flex-start")
-            if right_aligned:
-                vbox_layout.align_items = "flex-end"
-            return VBox([header, *children], layout = vbox_layout)
+        if self._notebook_number is not None:
+            image_filename = f"{self._notebook_number:0>2}-image{self._next_image_number:0>2}.png"
+            display_html(f"<img style='float: left;' src='../images/{image_filename}'>", raw = True)
+            self._next_image_number += 1
+            return
 
         random_button_hbox = HBox([self._random_button, self._random_button_int_text])
         upper_ui_widget_row = HBox([
-            vbox_with_header("Canvas", (self._clear_button, random_button_hbox)),
-            vbox_with_header("Animation", (self._animation_checkbox, self._animation_speed_slider)),
+            self._vbox_with_header("Canvas", (self._clear_button, random_button_hbox)),
+            self._vbox_with_header("Animation", (self._animation_checkbox, self._animation_speed_slider)),
         ], layout = Layout(margin = self._DEFAULT_VBOX_ITEM_MARGIN))
 
         lower_ui_widget_row = HBox([
-            vbox_with_header("Examples", self._example_buttons),
-            vbox_with_header("Algorithms", self._algorithm_buttons),
-            vbox_with_header("Messages", self._message_labels, right_aligned = True)
+            self._vbox_with_header("Examples", self._example_buttons),
+            self._vbox_with_header("Algorithms", self._algorithm_buttons),
+            self._vbox_with_header("Messages", self._message_labels, right_aligned = True)
         ])
 
         display(
@@ -178,9 +180,11 @@ class VisualisationTool(Generic[T]):
             ], layout = Layout(justify_content = "space-around"))
         )
 
-    """ def display(self, number = [2]):
-        display_html(f"<img style='float: left;' src='../images/convex-hull-{number[0]}.png'>", raw = True)
-        number[0] += 1 """
+    @staticmethod
+    def _vbox_with_header(title: str, children: Iterable[Widget], right_aligned: bool = False) -> VBox:
+        header = HTML(f"<h2>{title}</h2>", layout = Layout(align_self = "flex-start"))
+        vbox_layout = Layout(padding = "0px 25px", align_items = "flex-end" if right_aligned else "flex-start")
+        return VBox([header, *children], layout = vbox_layout)
 
     def add_point(self, point: Point) -> bool:
         if self._number_of_points >= 999:
@@ -212,7 +216,7 @@ class VisualisationTool(Generic[T]):
     def clear(self):
         self.clear_instance()
         self.clear_algorithm_output()
-        self._clear_message_labels()
+        self.clear_message_labels()
 
     def clear_instance(self):
         self._instance.clear()
@@ -224,7 +228,7 @@ class VisualisationTool(Generic[T]):
         if self._current_algorithm_drawer is not None:
             self._current_algorithm_drawer.clear()
 
-    def _clear_message_labels(self):
+    def clear_message_labels(self):
         for label in self._message_labels:
             label.value = "<br>"
 
@@ -262,17 +266,17 @@ class VisualisationTool(Generic[T]):
             layout = Layout(margin = self._DEFAULT_VBOX_ITEM_MARGIN)
         button = Button(description = description, layout = layout)
         def button_callback(_: Button):
-            self._disable_widgets()
+            self.disable_widgets()
             callback()
-            self._enable_widgets()
+            self.enable_widgets()
             self._previous_callback_finish_time = time.time()
         button.on_click(button_callback)
         return button
 
-    def _disable_widgets(self):
+    def disable_widgets(self):
         for widget in self._activatable_widgets:
             widget.disabled = True
 
-    def _enable_widgets(self):
+    def enable_widgets(self):
         for widget in self._activatable_widgets:
             widget.disabled = False
